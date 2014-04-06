eb350a5ab3a1d8de0c1e6f1e2e8f6355ca49f665
Sinelga <support@sinelga.com>
2014-04-06 04:12:55 +0300

    prod

--- goFastCgiLight/crongofastscript_example.bash
+++ goFastCgiLight/crongofastscript_example.bash
@@ -3,7 +3,7 @@
 #*/5 * * * * /home/juno/git/goFastCgi/goFastCgi/crongofastscript.bash
 #*/5 * * * * flock -n /tmp/crongofastscript.lock -c /home/juno/git/goFastCgi/goFastCgi/crongofastscript.bash
 
-cd /home/juno/git/goFastCgi/goFastCgi
+cd /home/juno/git/goFastCgiLight/goFastCgiLight
 #pgrep elabqueue && echo `date >>/tmp/elabqueue`
 #bin/paragraphshandler -locale=fi_FI -themes=finance -quant=500
 #pgrep elabqueue || bin/elabqueue
@@ -12,7 +12,7 @@ cd /home/juno/git/goFastCgi/goFastCgi
 #bin/elabqueue
 #bin/cleanupspace -hits=1 -created=300
 #pgrep elabqueue || bin/elabqueue && bin/cleanupspace -hits=1 -created=300 && bin/orphans -updated=1800
-pgrep elabqueue || pgrep cleanupspace || pgrep orphans || bin/elabqueue
+pgrep elabqueue || bin/elabqueue
 
 #bin/newdomain -locale=fi_FI -themes=porno -domain=tissit.tv -expire=3600
 #bin/newdomain -locale=fi_FI -themes=porno -expire=120
