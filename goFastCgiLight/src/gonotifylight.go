55d55d1290b2fbf58849fe296e385362eb021508
juno <juno@krasava.fi>
2014-04-28 05:36:34 +0300

    prod

--- goFastCgiLight/src/gonotifylight.go
+++ goFastCgiLight/src/gonotifylight.go
@@ -8,12 +8,15 @@ import (
 	"log/syslog"
 	"os"
 	"path/filepath"
+	"pushinqueue"
+	"strings"
 )
 
 const APP_VERSION = "0.1"
 
 // The flag package provides a default help printer via -h switch
 var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+var watcher *inotify.Watcher
 
 func main() {
 	golog, err := syslog.New(syslog.LOG_ERR, "golog")
@@ -30,7 +33,7 @@ func main() {
 
 	}
 
-	watcher, err := inotify.NewWatcher()
+	watcher, err = inotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +51,28 @@ func main() {
 	for {
 		select {
 		case ev := <-watcher.Event:
-			log.Println("event:", ev)
+			log.Println("event:", ev.String(), ev.Mask)
+
+			if ev.Mask == 1073742080 {
+				fmt.Println("Create dir ", ev.Name)
+				//				watcher.AddWatch(ev.Name,inotify.IN_CREATE|inotify.IN_ISDIR|inotify.IN_CLOSE_NOWRITE|inotify.IN_DELETE)
+				watcher.AddWatch(ev.Name, inotify.IN_ALL_EVENTS)
+				err = filepath.Walk(ev.Name, scan)
+				if err != nil {
+
+					golog.Err(err.Error())
+				}
+
+			}
+			if ev.Mask == 16 {
+				fmt.Println("Hit on ", ev.Name)
+				pushHit(*golog, ev.Name)
+			}
+			if ev.Mask == 1073742336 {
+				fmt.Println("delete dir ", ev.Name)
+				watcher.RemoveWatch(ev.Name)
+			}
+
 		case err := <-watcher.Error:
 			log.Println("error:", err)
 		}
@@ -58,11 +82,37 @@ func main() {
 func scan(path string, fileInfo os.FileInfo, inpErr error) (err error) {
 
 	if fileInfo.IsDir() {
-	
-		fmt.Println("dir ",fileInfo.Name())
-	
-	
+
+		//		if fileInfo.Name() != "www" {
+
+		//			watcher.AddWatch(path,inotify.IN_CREATE| inotify.IN_ISDIR|inotify.IN_CLOSE_NOWRITE|inotify.IN_DELETE)
+		watcher.AddWatch(path, inotify.IN_ALL_EVENTS)
+
+		//		}
+
 	}
 
 	return
+}
+
+func pushHit(golog syslog.Writer, path string) {
+
+	htmlfilesp := strings.Split(path, "/")
+	locale := htmlfilesp[1]
+	themes := htmlfilesp[2]
+	host := htmlfilesp[3]
+
+	var pathinfo string = "/"
+
+	for i := 4; i < len(htmlfilesp); i++ {
+
+		pathinfo = pathinfo + htmlfilesp[i]
+
+		if i < (len(htmlfilesp) - 1) {
+			pathinfo = pathinfo + "/"
+		}
+	}
+	fmt.Println(locale, themes, host, pathinfo)
+	pushinqueue.PushInQueue(golog, "redis", locale, themes, host, pathinfo)
+
 }
