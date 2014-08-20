583e708cbc1802e1c7d2f0fae41ebcf79da599f1
juno <juno@krasava.fi>
2014-08-20 05:47:22 +0300

    prod

--- goFastCgiLight/monitcleanup_example.bash
+++ goFastCgiLight/monitcleanup_example.bash
@@ -2,6 +2,7 @@ cd /home/juno/git/goFastCgiLight/goFastCgiLight
 
 /sbin/stop gonotifylight
 su juno -c "pgrep elabqueue || pgrep cleanup || bin/cleanup"
+su juno -c "pgrep find || /usr/bin/find www -type f -mtime -1 -size -10k -delete
 su juno -c "pgrep find || /usr/bin/find www -type d -empty -delete"
 su juno -c "/usr/bin/touch cleanupspace.csv"
 /sbin/start gonotifylight
