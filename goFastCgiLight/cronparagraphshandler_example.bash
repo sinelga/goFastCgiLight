2231b287e68ad0e765db217b43e19f75b8a6e67b
juno <juno@krasava.fi>
2014-08-27 02:57:55 +0300

    prod

--- goFastCgiLight/cronparagraphshandler_example.bash
+++ goFastCgiLight/cronparagraphshandler_example.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 #chmod +x!! must be done
 #*/5 * * * * /home/juno/git/goFastCgi/goFastCgi/cronparagraphshandler.bash
-#*/2 * * * * flock -n /tmp/cronparagraphshandler.lock -c /home/juno/git/goFastCgi/goFastCgi/cronparagraphshandler.bash
+#*/2 * * * * flock -n /tmp/cronparagraphshandler.lock -c /home/juno/git/goFastCgiLight/goFastCgiLight/cronparagraphshandler.bash
 
 cd /home/juno/git/goFastCgiLight/goFastCgiLight
 bin/paragraphshandler -locale=fi_FI -themes=porno -quant=3000
