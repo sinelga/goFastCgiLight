d32edd65523714295d45caa331e698ed340e1f23
juno <juno@krasava.fi>
2014-10-04 06:08:24 +0300

    prod

--- goFastCgiLight/src/bthandler/bthandler.go
+++ goFastCgiLight/src/bthandler/bthandler.go
@@ -20,7 +20,7 @@ func BTrequestHandler(golog syslog.Writer, resp http.ResponseWriter, req *http.R
 	}
 
 	var bytepage []byte
-	if strings.HasSuffix(pathinfoclean, ".html") || strings.HasSuffix(pathinfoclean, ".php") || strings.HasSuffix(pathinfoclean, ".jsp") {
+	if strings.HasSuffix(pathinfoclean, ".html")  {
 
 		bytepage = createpage.CreateHtmlPage(golog, locale, themes, bot, startparameters,blocksite)
 
