cf52ab4c75672446375c00aaa7e10b934b9d53b0
juno <juno@krasava.fi>
2014-10-17 05:57:10 +0300

    prod

--- goFastCgiLight/src/homepage/createhomepages/createhomepages.go
+++ goFastCgiLight/src/homepage/createhomepages/createhomepages.go
@@ -3,10 +3,11 @@ package createhomepages
 import (
 	"bytes"
 	"domains"
-//	"fmt"
+	//	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log/syslog"
+	"os"
 	"templ_funcmap"
 )
 
@@ -14,32 +15,55 @@ func CreatePages(golog syslog.Writer, sitesmap map[string]domains.Sitetohomepage
 
 	var base string
 	var page string
-//	var mediablock string
+	//	var mediablock string
 
 	base = "/home/juno/git/goFastCgiLight/goFastCgiLight/templ/_base.html"
 	page = "/home/juno/git/goFastCgiLight/goFastCgiLight/templ/homepageindex.html"
-//	mediablock = "/home/juno/git/goFastCgiLight/goFastCgiLight/templ/mediablock.html"
+	//	mediablock = "/home/juno/git/goFastCgiLight/goFastCgiLight/templ/mediablock.html"
 
 	funcMap := template.FuncMap{
-		"FirstWord":                  templ_funcmap.FirstWord,
-//		"FirstWordFromSenteces":      templ_funcmap.FirstWordFromSenteces,
-//		"FirstWordFromAllParagraphs": templ_funcmap.FirstWordFromAllParagraphs,
+		"FirstWord": templ_funcmap.FirstWord,
+		//		"FirstWordFromSenteces":      templ_funcmap.FirstWordFromSenteces,
+		//		"FirstWordFromAllParagraphs": templ_funcmap.FirstWordFromAllParagraphs,
 		"SplitPathOnWords": templ_funcmap.SplitPathOnWords,
-		"SplitDomainName": templ_funcmap.SplitDomainName,
-		"SomeSentences": templ_funcmap.SomeSentences,
+		"SplitDomainName":  templ_funcmap.SplitDomainName,
+		"SomeSentences":    templ_funcmap.SomeSentences,
 	}
 
 	index, _ := template.New("base").Funcs(funcMap).ParseFiles(
 		base,
 		page,
-//		mediablock,
+		//		mediablock,
 	)
 
 	for _, siteinfo := range sitesmap {
 
-		
-		indexpagefullpath :="/home/juno/git/goFastCgiLight/goFastCgiLight/www/"+siteinfo.Locale+"/"+siteinfo.Themes+"/"+siteinfo.Site+"/index.html"
-				
+		indexdirectory := "/home/juno/git/goFastCgiLight/goFastCgiLight/www/" + siteinfo.Locale + "/" + siteinfo.Themes + "/" + siteinfo.Site
+
+		//		src, err := os.Stat(indexdirectory)
+		if _, err := os.Stat(indexdirectory); os.IsNotExist(err) {
+
+			if os.MkdirAll(indexdirectory, 0777) != nil {
+				golog.Err(err.Error())
+
+			}
+
+		}
+
+		//		if err != nil {
+		//
+		//			golog.Err(err.Error())
+		//		}
+		//		if !src.IsDir() {
+		//
+		//			  if os.MkdirAll(indexdirectory, 0777) != nil {
+		//			  	golog.Err(err.Error())
+		//
+		//			  }
+		//		}
+
+		indexpagefullpath := "/home/juno/git/goFastCgiLight/goFastCgiLight/www/" + siteinfo.Locale + "/" + siteinfo.Themes + "/" + siteinfo.Site + "/index.html"
+
 		webpage := bytes.NewBuffer(nil)
 
 		if err := index.Execute(webpage, siteinfo); err != nil {
@@ -49,7 +73,7 @@ func CreatePages(golog syslog.Writer, sitesmap map[string]domains.Sitetohomepage
 		webpagebytes := make([]byte, webpage.Len())
 		webpagebytes = webpage.Bytes()
 
-		if err := ioutil.WriteFile(indexpagefullpath , webpagebytes, 0666); err != nil {
+		if err := ioutil.WriteFile(indexpagefullpath, webpagebytes, 0666); err != nil {
 			golog.Crit(err.Error())
 		}
 
---
+++ goFastCgiLight/src/startsite.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"domains"
+	"findfreeparagraph"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log/syslog"
+	"strings"
+	"homepage/createhomepages"
+)
+
+const APP_VERSION = "0.1"
+
+// The flag package provides a default help printer via -h switch
+var versionFlag *bool = flag.Bool("v", false, "Print the version number.")
+
+var startparameters []string
+
+var golog, _ = syslog.New(syslog.LOG_ERR, "golog")
+
+var localeFlag = flag.String("locale", "", "must be fi_FI/en_US/it_IT")
+var themesFlag = flag.String("themes", "", "must be porno/finance/fortune...")
+var siteFlag = flag.String("site", "", "any valid domain ")
+var paragraph domains.Paragraph
+var sitesmap map[string]domains.Sitetohomepage
+
+func main() {
+
+	flag.Parse() // Scan the arguments list
+
+	if *versionFlag {
+		fmt.Println("Version:", APP_VERSION)
+	}
+
+	locale := *localeFlag
+	themes := *themesFlag
+	site := *siteFlag
+
+	content, err := ioutil.ReadFile("/home/juno/git/goFastCgiLight/goFastCgiLight/config.txt")
+	if err != nil {
+		//Do something
+		golog.Err(err.Error())
+	}
+	parameters := strings.Split(string(content), ",")
+	startparameters = []string{strings.TrimSpace(parameters[0]), strings.TrimSpace(parameters[1]), strings.TrimSpace(parameters[2])}
+	
+	sitesmap = make(map[string]domains.Sitetohomepage)
+
+	for i := 0; i < 10; i++ {
+
+		paragraph = findfreeparagraph.FindFromQ(*golog, "fi_FI", "porno", "google", startparameters)
+
+		mapkey := locale + themes + site
+
+		_, ok := sitesmap[mapkey]
+
+		if !ok {
+
+			sitesmap[mapkey] = domains.Sitetohomepage{
+
+				Locale:    locale,
+				Themes:    themes,
+				Site:      site,
+				Pages:     []string{paragraph.Plocallink},
+				Paragraph: paragraph,
+			}
+
+		} else {
+			
+				pages := append(sitesmap[mapkey].Pages, paragraph.Plocallink)
+				paragraph = sitesmap[mapkey].Paragraph
+					sitesmap[mapkey] = domains.Sitetohomepage{
+					Locale:    locale,
+					Themes:    themes,
+					Site:      site,
+					Pages:     pages,
+					Paragraph: paragraph,
+				}
+
+		}
+
+	}
+	
+	createhomepages.CreatePages(*golog, sitesmap)
+		
+
+}
